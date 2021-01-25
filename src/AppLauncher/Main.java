1cb7e16cb20c6aea57a7ee367f941d17f9d2adb6
Hubert <hugraetz@live.com>
2021-01-25 17:21:07 +0100

    Filechooser button

--- src/AppLauncher/Main.java
+++ src/AppLauncher/Main.java
@@ -4,16 +4,38 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+
 public class Main extends Application {
+    private FileChooser fileChooser;
+    Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        this.primaryStage = primaryStage;
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        fileChooser = new FileChooser();
+
+        Button button = new Button("select File");
+        button.setOnAction(event -> doSmth());
+
+        VBox vBox = new VBox(button);
+
+        primaryStage.setScene(new Scene(vBox, 300, 275));
         primaryStage.show();
+    }
+
+    private void doSmth() {
+
+        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+
     }
 
 
